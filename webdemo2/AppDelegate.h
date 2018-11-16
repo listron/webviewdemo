c019dea03c1c6703cb9f45c7c217c3583fdb308b
listron <tjbdzq@163.com>
2018-11-16 11:08:54 +0800

    Initial Commit

---
+++ webdemo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  webdemo2
+//
+//  Created by zhangqiang on 2018/11/16.
+//  Copyright © 2018年 zhangqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ webdemo2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  webdemo2
+//
+//  Created by zhangqiang on 2018/11/16.
+//  Copyright © 2018年 zhangqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
